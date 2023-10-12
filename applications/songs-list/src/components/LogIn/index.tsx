faf7df35a33ff9bcea6fa0bcf0fa0ff7574f952f
Bruno Assis <bruno.assis@choco.com>
2023-10-12 15:33:42 +0200

    clean up

--- applications/songs-list/src/components/LogIn/index.tsx
+++ applications/songs-list/src/components/LogIn/index.tsx
@@ -3,16 +3,10 @@ import { Button } from "../Button";
 import styles from "./login.module.css";
 
 export const LogIn = (): JSX.Element => {
-  const callServer = () => {
-    fetch("http://localhost:4441/user/1")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  };
-
   return (
     <div className={styles.container}>
       <h1>Log in to see your favourite songs!</h1>
-      <Button onClick={callServer}>Log in</Button>
+      <Button>Log in</Button>
     </div>
   );
 };
