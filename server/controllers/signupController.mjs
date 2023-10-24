3b2996d69c527aa9c9bf14138b4dda8f75287ee6
Bruno Assis <bruno.assis@choco.com>
2023-10-24 13:45:38 +0200

    fix signup controller

--- server/controllers/signupController.mjs
+++ server/controllers/signupController.mjs
@@ -5,22 +5,21 @@ const router = express.Router();
 
 router.route("/").post((req, res) => {
   const users = JSON.parse(readFileSync("./fixtures/users.json", "utf8"));
-  const user = users.find((user) => user.credentials.email === req.body.email);
+  const user = users.find(
+    (x) => x.credentials.email === req.body?.credentials?.email
+  );
 
   if (user) {
-    res.boom.badRequest("User already exists");
+    res.boom.badData("User already exists");
   } else {
     try {
       const newUser = userSchema.parse(req.body);
       newUser.id = String(users.length + 1);
-
       users.push(newUser);
-
       writeFileSync("./fixtures/users.json", JSON.stringify(users, null, 2));
-
       res.send(newUser);
     } catch (error) {
-      res.boom.badData(error.message);
+      res.boom.badData("Invalid data", error);
     }
   }
 });
